d7bb4204720301f9d2011ec2587b85e7a7be1e22
deiviRep <patzivargasdavid@gmail.com>
2025-08-04 09:58:11 -0400

    desafio amigo secreto funcionando

--- app.js
+++ app.js
@@ -1,1 +1,43 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = []
+
+function agregarAmigo(){
+    let nombre = document.getElementById("amigo").value;
+    if (!nombre.trim()) {
+        alert("Por favor, inserte un nombre");
+        return;
+    }
+    amigos.push(nombre);
+    limpiarCaja();
+    actualizarListaAmigos();
+    return;
+}
+
+function limpiarCaja(){
+    document.getElementById("amigo").value = '';
+}
+
+function actualizarListaAmigos(){
+    let lista = document.getElementById("listaAmigos");
+    lista.innerHTML = "";
+    for (let i = 0; i < amigos.length; i++) {
+        const amigo = amigos[i];
+        lista.innerHTML += `<li>${amigo}</li>`
+    }
+    return;
+}
+
+function sortearAmigo(){
+    if (amigos.length === 0) {
+        alert("Deves, insertar los amigos");
+        return;
+    }
+    const numeroAleatorio = generaNumeroAleatorio(amigos.length)
+    let listaSorteo = document.getElementById("resultado");
+    listaSorteo.innerHTML = `<li>${amigos[numeroAleatorio]}</li>`
+    return;
+}
+
+function generaNumeroAleatorio(limite){
+    return Math.floor(Math.random()*limite);
+}
